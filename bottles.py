35a07e57502f8dc967a119af490f96d42e8051a5
annascriv <scriven.anna@outlook.com>
2023-10-05 20:34:43 -0500

    added 99 bottles song

--- bottles.py
+++ bottles.py
@@ -1,5 +1,16 @@
-def bottle_song():
-	# write your code here!
-	pass
+def bottle_song(num):
+	#num = 99
+	song = ''
+	while num>0:
+		song+=f'''{num} bottles of beer on the wall, {num} bottles of beer. Take one down and pass it around, {num-1} bottles of beer on the wall.'''
+		num=num-1
 
-bottle_song()
+		if num == 2:
+			song+=f'''{num} bottles of beer on the wall, {num} bottles of beer. Take one down and pass it around, {num-1} bottle of beer on the wall.'''
+			num=num-1
+		elif num == 1:
+			song+=f'''{num} bottle of beer on the wall, {num} bottle of beer. Take it down and pass it around, no bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, no bottles of beer on the wall.'''
+	
+	return song
+
+print(bottle_song(6))
